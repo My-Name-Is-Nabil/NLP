ffbdbe8da7cb8447df4e943c7a7f75171a37fb92
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-05-11 02:43:35 +0200

    API call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,18 +2,27 @@ import { displaySuccess } from './displaySuccess';
 import { formChecker } from './formChecker';
 function handleSubmit(event) {
     event.preventDefault();
-
     // check what text was put into the form field
-    let formText = document.getElementById('article').value;
-    const success = formChecker(formText);
+    let article = document.getElementById('article').value;
+    let processor = document.getElementById('processers').value;
+    const success = formChecker(article);
     if (success){
-        fetch('http://localhost:8081/api')
-        .then(res => {
-            displaySuccess();
-            return res.json();
-        }, err => console.log(err))
+        fetch('http://localhost:8081/api', {
+            method:'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                'article': article,
+                'processor': processor
+            })
+        })
+        .then(res => res.json() , err => console.log(err))
         .then(function(res) {
             console.log(res);
+            if (res.status.code == 0 ){
+                displaySuccess();
+            }
             document.querySelector('.result__response').innerHTML = res.message;
         } , err => console.log(err));
     }
--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors');
 const app = express()
-
-
+const axios = require('axios');
 require('dotenv').config();
+
 app.use(express.static('dist'))
 app.use(cors());
+app.use(express.json());
 const API_KEY = process.env.API_KEY;
 
 console.log(API_KEY);
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -23,6 +23,56 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/api', function (req, res) {
-    res.send(mockAPIResponse)
+app.post('/api', function (req, res) {
+    const processor = req.body.processor;
+    const article = req.body.article;
+    if (processor == 'sentiment-analysis'){
+        axios.get('https://api.meaningcloud.com/sentiment-2.1',{
+            params:{
+                key:API_KEY,
+                lang:'en',
+                model:'general',
+                txt:article,
+            }      
+        }).then(response=>{
+            console.log(response.data);
+            res.send(JSON.stringify(response.data));
+        }).catch(error=>{
+            console.error(error);
+            res.send(error);
+        })
+    }
+    else if (processor == 'summarization'){
+        axios.get('https://api.meaningcloud.com/summarization-1.0',{
+            params:{
+                key:API_KEY,
+                of:'json',
+                txt:article,
+                sentences: 2
+            }
+        }).then(response=>{
+            console.log(response.data);
+            res.send(JSON.stringify(response.data));
+        }).catch(error=>{
+            console.error(error);
+            res.send(error);
+        })
+    }
+    else if (processor == 'topic-extraction'){
+        axios.get('https://api.meaningcloud.com/topics-2.0',{
+            params:{
+                key:API_KEY,
+                lang:'en',
+                tt:'a',
+                uw:'y',
+                txt:article,
+            }
+        }).then(response=>{
+            console.log(response.data);
+            res.send(JSON.stringify(response.data));
+        }).catch(error=>{
+            console.error(error);
+            res.send(error);
+        })
+    }
 })
